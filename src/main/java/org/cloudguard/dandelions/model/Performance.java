6fe82acc9a32ebb4dd062ca1ced3e5a523cf5811
ccdtrd <ccdtrd@gmail.com>
2016-12-05 00:27:36 -0500

    refactor

--- src/main/java/org/cloudguard/dandelions/model/Performance.java
+++
@@ -1,9 +0,0 @@
-package org.cloudguard.dandelions.model;
-
-public class Performance {
-	
-	public String id;
-	public int time;
-	public int reps;
-	
-}
--- src/main/resources/static/js/hello.js
+++
@@ -1,7 +0,0 @@
-angular.module('hello', [])
-  .controller('home', function($http) {
-  var self = this;
-  $http.get('/resource/').then(function(response) {
-    self.greeting = response.data;
-  })
-});
