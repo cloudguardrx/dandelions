76d2acd4da84f22f7038df336b266a2bd116b6e6
Travis Luecke <travisluecke@Traviss-MacBook-Pro.local>
2017-03-25 16:05:55 -0400

    Fixed mongo config

--- src/main/java/org/cg/dandelions/config/MongoConfig.java
+++ src/main/java/org/cg/dandelions/config/MongoConfig.java
@@ -1,19 +1,36 @@
 package org.cg.dandelions.config;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 
+@Configuration
 @Component
 public class MongoConfig {
+	
+    @Value("${mongo.uri}")
+    public String mongoUri;
+    
+    @Value("${mongo.database}")
+    public String mongoDatabase;
 
 	public MongoClient client;
 	public MongoDatabase database;
 
 	public MongoConfig() {
-		client = new MongoClient("localhost");
-		database = client.getDatabase("dandelion");
+	}
+	
+	@PostConstruct
+	public void init() {
+		this.client = new MongoClient(new MongoClientURI(mongoUri));
+		this.database = client.getDatabase(mongoDatabase);
 	}
 
 }
